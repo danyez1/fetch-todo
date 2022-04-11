b6c58c6e79cf56b61f15b8f5c2f0127eef592c5a
danyez1 <cdanielyanez144@gmail.com>
2022-04-11 23:54:05 +0000

    initial

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,53 @@
+import { Redirect } from "react-router-dom";
+
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {
+      list: [],
+    },
+    actions: {
+      getData: () =>{ 
+        fetch(
+          "https://assets.breatheco.de/apis/fake/todos/user/cruzdanielyanez")
+          .then((response) => response.json())
+          .then((result) => setStore({ list: result }))
+          .catch((error) => console.log("error", error))
+    },
+    addItem: (item) => {
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/cruzdanielyanez", {
+        method: "PUT",
+        redirect: "follow",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([
+          ...getStore().list,
+          { label: item, done: false },
+        ]),
+      })
+        .then((response) =>
+          response.status == 200 ? getActions().getData() : ""
+        )
+        .catch((error) => console.log("error", error))
+    },
+	deleteItem: (list) => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/cruzdanielyanez", {
+		  method: "PUT",
+		  redirect: "follow",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(list),
+		})
+		  .then((response) =>
+			response.status == 200 ? getActions().getData() : ""
+		  )
+		  .catch((error) => console.log("error", error))
+	  },
+	}
+  };
 };
+
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,71 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import { element } from "prop-types";
+import React, { useContext, useState } from "react";
 import "../../styles/home.css";
+//import { useContext, useState } from "react/cjs/react.production.min";
+import { Context } from "../store/appContext.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  const [item, setItem] = useState("");
+
+  return (
+    <div className="container text-center mt-5">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="New task"
+          onChange={(event) => {
+            setItem(event.target.value);
+          }}
+          value={item}
+        />
+      </div>
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+        onClick={() => {
+          if (item !== "") {
+            actions.addItem(item);
+            setItem("");
+          }
+        }}
+      >
+        Submit
+      </button>
+
+      <ul className="list-group">
+        {store.list.map((element, index) => {
+          return (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-around justify-content-center p-2  bg-light"
+            >
+              {element.done ? (
+                <p className="align-self-end text-dark fs-3 mb-0">
+                  {element.label}
+                </p>
+              ) : (
+                <p className="ms-5 text-primary">{element.label}</p>
+              )}
+
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={() => {
+                  actions.deleteItem(
+                    store.list.filter((item, i) => i !== index)
+                  );
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Home;
